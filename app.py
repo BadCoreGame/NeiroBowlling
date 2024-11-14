2195773c57af4795124431b61f787a206b52b721
roman <gamemusik512@gmail.com>
2024-11-14 17:54:47 +0700

    v0.3

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
@@ -10,18 +11,44 @@ try:
 except Exception as e:
     print("Ошибка загрузки YOLOv11 модели:", e)
 
-# Хранение загруженных файлов 
+# Хранение загруженных файлов
 loaded_images = []
 gotov_image = {}
+
+# Добавьте переменные для обработки кликов
+click_count = 0
+click_timer = None
+
+def on_click(event):
+    global click_count, click_timer
+
+    click_count += 1
+
+    if click_count == 1:
+        click_timer = root.after(250, reset_click_count)  # Установите таймер на 250 мс
+    elif click_count == 2:
+        root.after_cancel(click_timer)  # Отмените таймер, если это двойной клик
+        look_image(loaded_images[window_list.curselection()[0]])  # Обработайте двойной клик
+        reset_click_count()
+
+def reset_click_count():
+    global click_count, click_timer
+    click_count = 0
+    if click_timer is not None:
+        root.after_cancel(click_timer)
+        click_timer = None
 
 def process_image():
     global loaded_images, gotov_image
     if not loaded_images:
         result_label.config(text="Пожалуйста, загрузите изображение.")
         return
-        
+
     for file_path in loaded_images:
         # Загружаем и обрабатываем изображение
+        if not os.path.exists(file_path):
+            print(f"Файл не найден: {file_path}")
+            continue
         img = Image.open(file_path)
         results = model(img)
         print(f"Изображение {file_path} обработано.")
@@ -31,7 +58,7 @@ def process_image():
         for i, r in enumerate(results):
             results_img_bgr = r.plot()  # BGR-order numpy array
             results_img = Image.fromarray(results_img_bgr[..., ::-1])  # RGB-order PIL image
-            
+
             results_img = results_img.resize((400, 400))
 
             img_tk = ImageTk.PhotoImage(results_img)
@@ -55,72 +82,135 @@ def process_image():
                 result_label.config(text=f"Боулинг не обнаружен на изображении {file_path}")
                 print(f"Боулинг не обнаружен на изображении {file_path}.")
 
-# Функция для загрузки изображений из массива
-def display_loaded_images():
-    for widget in images_frame.winfo_children():
-        widget.destroy()  # Удаляем предыдущие элементы
+# Просмотр изображений
+def look_image(file_path):
+    """Display the original image in the canvas."""
+    img = Image.open(file_path)
+    img = img.resize((400, 400))  # Изменяем размер изображения на 400x400
+    img_tk = ImageTk.PhotoImage(img)
     
-    for file_path in loaded_images:
-        file_name = file_path.split("/")[-1]  # Получаем имя файла из пути
-        img = Image.open(file_path)
-        img.thumbnail((100, 100))  # Уменьшаем изображение для отображения в списке
-        img_tk = ImageTk.PhotoImage(img)
-        
-        # Создаем метку для имени файла
-        name_label = tk.Label(images_frame, text=file_name, bg='white', fg='black')
-        name_label.pack(side=tk.TOP)
-
-        # Создаем метку для изображения
-        img_label = tk.Label(images_frame, image=img_tk, bg='white')
-        img_label.image = img_tk  # Сохраняем ссылку на изображение
-        img_label.pack(side=tk.TOP)
+    # Очищаем canvas перед отображением нового изображения
+    canvas.delete(tk.ALL)
+    canvas.create_image(0, 0, anchor="nw", image=img_tk)
+    canvas.image = img_tk  # Сохраняем ссылку на изображение
 
 # Функция для загрузки изображений
 def upload_images():
     """Upload images and display them."""
+    global loaded_images, method_var
+
     global loaded_images
-    file_paths = filedialog.askopenfilenames(title="Выберите изображения.",
-    
-    filetypes=[("Images file", "*.jpg;*.jpeg;*.png;*.ico;*.bmp;*.tif;*.pcx;*")])  # Выбор файлов
-    
-    loaded_images = file_paths
+    if method_var.get() == "files":
+        file_paths = filedialog.askopenfilenames(
+            title="Выберите изображения.",
+            filetypes=[("Images file", "*.jpg;*.jpeg;*.png;*.ico;*.bmp;*.tif;*.pcx;*")]
+        )
+
+        if file_paths:
+            loaded_images = file_paths
+    elif method_var.get() == "folder":
+        folder_path = filedialog.askdirectory(title="Выберите папку с изображениями.")
+        if folder_path:
+            loaded_images = []
+            for root, dirs, files in os.walk(folder_path):
+                for file in files:
+                    if file.lower().endswith((".jpg", ".jpeg", ".png", ".ico", ".bmp", ".tif", ".pcx")):
+                        loaded_images.append(os.path.join(root, file))  # Сохраняем полные пути к файлам
+
     print(f"Изображения загружены: {loaded_images}")
     if loaded_images:
         result_label.config(text="")
-        display_loaded_images()
+        window_list.delete(0, tk.END)  # Удаляем предыдущие элементы
+        for file_path in loaded_images:
+            file_name = os.path.basename(file_path)  # Получаем имя файла из пути
+            window_list.insert(tk.END, file_name)
+                
 
-# Создаем основное окно -------------
+# Создаем основное окно --------------------------------------------------
 backg='#222831'  # Установка цвета фона
+text_color = '#ffffff'  # Цвет текста
+button_bg = '#444950'  # Цвет фона кнопок
+button_fg = '#ffffff'  # Цвет текста кнопок
+
 root = tk.Tk()
-root.title("NeiroBowling")
+root.title("NeiroBowlling_AI")
+root.iconphoto(False, tk.PhotoImage(file="./icno.png"))
 
 w = root.winfo_screenwidth()
 h = root.winfo_screenheight()
 w = w // 2  # середина экрана
 h = h // 2
-w = w - 500  # смещение от середины
+w = w - 350  # смещение от середины
 h = h - 300
-
-root.geometry(f'1000x600+{w}+{h}')
+root.geometry(f'800x700+{w}+{h}')
 root.resizable(False, False)
 root.configure(bg=backg)
 
+# разметка окна -------------------------------
+
+# правая сторона окна----
+frame_right = tk.LabelFrame(width="500", bg=backg)
+frame_right.pack(fill="y", side=tk.RIGHT)
 # Создаем элементы интерфейса
-images_frame = tk.Frame(root, bg=backg)
-images_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-canvas = tk.Canvas(root, bg=backg, width=500, height=600)
-canvas.pack(side=tk.RIGHT)
+canvas = tk.Canvas(frame_right, bg=backg, width="400", height="400")
+canvas.pack(side=tk.TOP, pady="20", padx="50")
 
-result_label = tk.Label(root, text="", bg=backg, fg='white')
-result_label.pack(side=tk.BOTTOM)
+result_label= tk.Label(frame_right, text="Ну ты и лох", bg=backg, fg='white')
+result_label.pack(side=tk.BOTTOM, fill="both", expand=True)
 
-# Кнопка загрузки изображений
-upload_button = tk.Button(root, text="Загрузить изображения", command=upload_images)
-upload_button.pack(side=tk.BOTTOM)
+# разделитель-----
+divine_line= tk.Label(bg="#f0f0f0", width="0")
+divine_line.pack(side=tk.RIGHT, fill="y")
+
+# левая сторона окна
+frame_left = tk.LabelFrame(bg=backg)
+frame_left.pack( side=tk.LEFT, fill="both", expand=True)
+# Создаем элементы интерфейса
+name = tk.Frame(frame_left, bg=backg, height="60")
+name.pack(side=tk.TOP, fill="x", ipady="5")
+name_label = tk.Label(name, text="NeiroBowling", bg=backg, fg='white', font=("Helvetica", 23, "bold"))
+name_label.pack(side=tk.LEFT, padx="7")
+
+name_img= Image.open("./logo.png")
+name_img.thumbnail((50, 50))
+name_img_tk= ImageTk.PhotoImage(name_img)
+name_img_label = tk.Label(name, image=name_img_tk, bg=backg)
+name_img_label.image = name_img_tk
+name_img_label.pack(side=tk.LEFT)
+
+
+explorer = tk.Frame(frame_left, bg=backg)
+explorer.pack(side=tk.TOP, fill="both", expand=True)
+window_list = tk.Listbox(explorer, bg=backg, fg="white")
+window_list.pack(side=tk.LEFT, fill="both", expand=True)
+window_list.bind("<Button-1>", on_click)
+spisok = tk.Scrollbar(explorer, orient="vertical", command=window_list.yview)
+spisok.pack(side=tk.RIGHT, fill="y")
+window_list.config(yscrollcommand=spisok.set)
+images_frame = tk.Frame(frame_left, bg=backg)
+images_frame.pack(side=tk.TOP, fill="both", expand=True)
+
+
 
 # Кнопка обработки изображений
-process_button = tk.Button(root, text="Обработать изображения", command=process_image)
-process_button.pack(side=tk.BOTTOM)
+process_button = tk.Button(frame_left, text="Обработать изображения", command=process_image, bg=button_bg, fg=button_fg, font=("Helvetica", 12))
+process_button.pack(side=tk.BOTTOM, pady=10)
+
+# Переменные для радиокнопок
+method_var = tk.StringVar(value="files")  # Значение по умолчанию - загрузка файлов
+
+    # Радиокнопки для выбора метода загрузки
+select_frame = tk.LabelFrame(frame_left, bg=backg, text="select", fg="white")
+select_frame.pack(side=tk.BOTTOM, fill="x", pady="10", padx="30")
+tk.Radiobutton(select_frame, text="Загрузить файлы", variable=method_var, value="files", bg=backg, fg=button_fg, font=("Helvetica", 12), selectcolor=button_bg).pack(anchor=tk.SW, pady="5", padx="5")
+tk.Radiobutton(select_frame, text="Загрузить папку", variable=method_var, value="folder", bg=backg, fg=button_fg, font=("Helvetica", 12), selectcolor=button_bg).pack(anchor=tk.SW, pady="5", padx="5")
+
+# Кнопка загрузки изображений
+upload_button = tk.Button(frame_left, text="Загрузить изображения", command=upload_images, bg=button_bg, fg=button_fg, font=("Helvetica", 12))
+upload_button.pack(side=tk.BOTTOM, pady=10)
 
 root.mainloop()
+
+
+# cursor="hand2"
